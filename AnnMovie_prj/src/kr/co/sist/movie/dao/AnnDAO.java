a7d0dea1a6d1a3b769db4f1e68ea830b3c98a290
akj1429 <akj14291@gmail.com>
2017-05-22 17:48:52 +0900

    dao설정 1st

--- AnnMovie_prj/src/kr/co/sist/movie/dao/AnnDAO.java
+++ AnnMovie_prj/src/kr/co/sist/movie/dao/AnnDAO.java
@@ -1,9 +1,62 @@
 package kr.co.sist.movie.dao;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import javax.swing.JOptionPane;
+
 public class AnnDAO {
 	private static AnnDAO a_dao;
 	
 	public AnnDAO() {
-	}
+	}//AnnDAO
 
-}
+	public static AnnDAO getInstance(){
+		
+		if(a_dao == null){
+			a_dao = new AnnDAO();
+		}
+		return a_dao;
+	}//getInstance
+	
+	private Connection getConnection() throws SQLException{
+		Connection con = null;
+
+		Properties prop = new Properties();
+		try {
+			File file = new File("경로 설정하세요~");
+			
+			if (file.exists()) {
+				prop.load(new FileInputStream(file));
+				String driver = prop.getProperty("driver");
+				String url = prop.getProperty("url");
+				String id = prop.getProperty("dboid");
+				String pass = prop.getProperty("dbopwd");
+
+				try {
+					Class.forName(driver);
+					con = DriverManager.getConnection(url, id, pass);
+				} catch (ClassNotFoundException e) {
+					e.printStackTrace();
+				} // end catch
+
+			} else {
+				JOptionPane.showMessageDialog(null, "설정파일의 경로를 확인해주세요");
+			} // end else
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} // end catch
+
+		return con;
+	}//getConnection
+	
+}//class
